1b99538f8c24ac07a03bc13420035809654d4a36
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-04-21 11:09:37 +0800

    refactor home page view to use a templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 # Create your tests here.
@@ -9,14 +9,27 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func,home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found=resolve('/')
+    #     self.assertEqual(found.func,home_page)
 
-    def test_home_page_returns_correct_htmL(self):
-        request=HttpRequest()
-        response=home_page(request)
-        html=response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.strip().endswith('</html>'))
+    # def test_home_page_returns_correct_htmL(self):
+    #     # request=HttpRequest()
+    #     # response=home_page(request)
+    #     response=self.client.get('/')           #检测的原生方式
+    #     html=response.content.decode('utf-8')
+    #
+    #     expected_html=render_to_string('home.html') #测试是否正确渲染模板，然后与视图返回的结果做对比 自定义检测，万能模式
+    #     self.assertEqual(html,expected_html)
+    #
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>',html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+    #
+    #     self.assertTemplateUsed(response,'home.html') #测试是否正确渲染模板，然后与视图返回的结果做对比   检测的原生方式
+    #
+    #     self.assertEqual(response,'wrong.html')
+    #
+    def test_users_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')

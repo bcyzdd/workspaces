ea830659460f2b8d33b747cd25201df7de5ca61b
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-04-21 23:08:44 +0800

    在服务器中处理post请求，调用self.client.post，传入data参数，指定想发送的单数据

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return render(request,'home.html')
+    # return render(request,'home.html')
     #使用Django中的render函数构建HttpResponse，第一个参数为请求对象，
     # 第二个是渲染模板，Django会自动搜索templates文件夹下，根据模板内容创建一个HttpResponse
+    if request.method=='POST':
+        return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html')
 

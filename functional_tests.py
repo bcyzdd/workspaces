00d590fda4fdbe1121e3759a13ec98f5cfa935d7
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-04-21 22:56:13 +0800

    模板标签，添加CSRF令牌

--- functional_tests.py
+++ functional_tests.py
@@ -26,7 +26,7 @@ class NewVistorTest(unittest.TestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
 
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')

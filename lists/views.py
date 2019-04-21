c458131749670f6bac489458b6850ee9dc58a004
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-04-21 23:21:48 +0800

    把python变量传入模板中的渲染

--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,9 @@ def home_page(request):
     # return render(request,'home.html')
     #使用Django中的render函数构建HttpResponse，第一个参数为请求对象，
     # 第二个是渲染模板，Django会自动搜索templates文件夹下，根据模板内容创建一个HttpResponse
-    if request.method=='POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request,'home.html')
-
+    # if request.method=='POST':
+    #     return HttpResponse(request.POST['item_text'])
+    # return render(request,'home.html')
+    return render(request,'home.html',{
+        'new_item_text':request.POST['item_text'],
+    })

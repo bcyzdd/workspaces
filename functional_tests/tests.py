c9f44ade8ef26c13bd2ba7cc629388691c5e2af2
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-05-06 23:51:40 +0800

    新增 list.html文件，修改view视图

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,15 +32,28 @@ class NewVistorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         header_text=self.browser.find_element_by_tag_name('h1').text
         inputbox=self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('1:Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+        self.wait_for_row_in_list_table('1：Buy peacock feathers')
         inputbox = self.browser.find_element_by_id('id_new_item')
         time.sleep(1)
-        inputbox.send_keys('2: Use peacock feathers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Fish the test!!')
 
+    def test_can_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1：Buy peacock feathers')
+
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists.+')
+        

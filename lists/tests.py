082443003bd5cba8793b238059cba31b4a65d982
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-05-09 22:56:59 +0800

    删除注释掉的无用代码

--- lists/tests.py
+++ lists/tests.py
@@ -2,33 +2,19 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from ..lists.models import Item
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-from lists.views import home_page
+from ..lists.views import home_page
 # Create your tests here.
 
 class HomePageTest(TestCase):
     def test_users_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
-    def test_can_save_a_POST_request(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
 
-        self.assertEqual(Item.objects.count(),1)#检查是否把一个新的item对象存入数据库，
-        # object.count（）是object.all（）.count（）的简写模式
-        new_item=Item.objects.first()#object.first（）等价于object.all()[0]
-        self.assertEqual(new_item,'A new list item')#检查待办事项的文本是否正确
-    def test_redirects_after_POST(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(),0)
 
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
@@ -71,3 +57,15 @@ class   ListViewTest(TestCase):
 
         self.assertContains(response,'itemey 1')
         self.assertContains(response,'itemey 2')
+class   NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(),1)#检查是否把一个新的item对象存入数据库，
+        # object.count（）是object.all（）.count（）的简写模式
+        new_item=Item.objects.first()#object.first（）等价于object.all()[0]
+        self.assertEqual(new_item,'A new list item')#检查待办事项的文本是否正确
+    def test_redirects_after_POST(self):
+        response=self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -1,41 +1,10 @@
 from django.shortcuts import render,redirect
-from lists.models import Item
-
-# Create your views here.
-
-# def home_page(request):
-#     # return render(request,'home.html')
-#     #使用Django中的render函数构建HttpResponse，第一个参数为请求对象，
-#     # 第二个是渲染模板，Django会自动搜索templates文件夹下，根据模板内容创建一个HttpResponse
-#     # if request.method=='POST':
-#     #     return HttpResponse(request.POST['item_text'])
-#     # return render(request,'home.html')
-#     # return render(request,'home.html',{
-#     #     'new_item_text':request.POST.get('item_text',''),
-#     # })
-#     item=Item()
-#     item.text=request.POST.get('item_text','')
-#     item.save()
-#
-#     return render(request,'home.html',{
-#         'new_item_text':item.text
-#     })
-
-
-# def home_page(request):
-#     if request.method == 'POST':
-#         new_item_text = request.POST['item_text']
-#         Item.objects.create(text=new_item_text)
-#     else:
-#         new_item_text = ''
-#     return render(request,'home.html',{
-#         'new_item_text':new_item_text,
-#     })
+from ..lists.models import Item
 def home_page(request):
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
 def view_list(request):
     items=Item.objects.all()
     return render(request,'list.html',{'itmes':items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')

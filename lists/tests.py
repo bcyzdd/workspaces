1544a151b3e50454539595dfebddba3e90fbc2ba
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-05-14 23:36:41 +0800

    7.10.2 按照新设计调整new_list

--- lists/tests.py
+++ lists/tests.py
@@ -86,9 +86,8 @@ class   NewListTest(TestCase):
         self.assertEqual(new_item,'A new list item')#检查待办事项的文本是否正确
     def test_redirects_after_POST(self):
         response=self.client.post('/lists/new',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
-        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+        new_list=List.objects.first()
+        self.assertRedirects(response,f'/lists/{new_list.id}')
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_=List.objects.create()

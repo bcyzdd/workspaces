1ec79987125bd67b32b905a2382eedd81e989f83
bcyzdd <31878313+bcyzdd@users.noreply.github.com>
2019-05-14 23:32:04 +0800

    7.10.1 捕获URL中的参数

--- lists/views.py
+++ lists/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render,redirect
 from lists.models import Item,List
 def home_page(request):
     return render(request,'home.html')
-def view_list(request):
-    items=Item.objects.all()
+def view_list(request,list_id):
+    list_=List.objects.get(id=list_id)
+    items=Item.objects.filter(list=list_)
     return render(request,'list.html',{'itmes':items})
 def new_list(request):
     list_=List.objects.create()
